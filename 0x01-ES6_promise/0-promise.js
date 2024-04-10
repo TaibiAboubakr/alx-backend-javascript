d603235e16f9500248e327730552d241c3ce9df7
aboubakr <c.aboubakr@gmail.com>
2024-04-10 16:04:32 +0000

    update the file 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,1 +1,15 @@
-#!/usr/bin/node
+export default function getResponseFromAPI() {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			const data = {
+				message: "Data fetched successfully from API",
+				payload: {
+					key1: "value1",
+					key2: "value2"
+				}
+			};
+			resolve(data);
+
+		}, 2000);
+	});
+}
