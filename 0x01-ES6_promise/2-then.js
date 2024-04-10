cfc42d89468bf8d38ef832be7e8f32cdc5955186
aboubakr <c.aboubakr@gmail.com>
2024-04-10 23:01:46 +0000

    update the file 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+export default function handleResponseFromAPI(promise) {
+  promise.then((response) => {
+    console.log('Got a response from the API');
+    return { status: 200, body: 'success' };
+  }).catch((error) => {
+    console.error('Got an error from the API');
+    return new Error();
+  });
+}
