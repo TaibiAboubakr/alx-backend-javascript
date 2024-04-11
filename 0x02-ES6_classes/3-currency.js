4c49e72ff85ec5af4c3ec943459649611f93f95f
aboubakr <c.aboubakr@gmail.com>
2024-04-11 23:21:54 +0000

    update the file 3-currency.js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,39 +1,39 @@
 class Currency {
   constructor(code, name) {
-	  this.code = code;
-	  this.name = name;
+    this.code = code;
+    this.name = name;
   }
 
   /**
-	 * @param {String} code
-	 */
+* @param {String} code
+*/
   set code(code) {
-	  if (typeof code !== 'string') {
+    if (typeof code !== 'string') {
       throw new TypeError('Code must be a String');
-	  }
-	  this._code = code;
+    }
+    this._code = code;
   }
 
   get code() {
-	  return this._code;
+    return this._code;
   }
 
   /**
-	 * @param {String} name
-	 */
+* @param {String} name
+*/
   set name(name) {
-	  if (typeof name !== 'string') {
+    if (typeof name !== 'string') {
       throw new TypeError('Name must be a String');
-	  }
-	  this._name = name;
+    }
+    this._name = name;
   }
 
   get name() {
-	  return this._name;
+    return this._name;
   }
 
   displayFullCurrency() {
-	  return `${this.name} (${this.code})`;
+    return `${this.name} (${this.code})`;
   }
 }
 
