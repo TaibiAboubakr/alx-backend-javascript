6260257aade7278d63307941239649125bff1dd1
aboubakr <c.aboubakr@gmail.com>
2024-04-11 23:20:19 +0000

    update the file 2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,51 +1,51 @@
 class HolbertonCourse {
   constructor(name, length, students) {
-	  this.name = name;
-	  this.length = length;
-	  this.students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   /**
-	 * @param {String} name
-	 */
+    * @param {String} name
+    */
   set name(name) {
-	  if (typeof name !== 'string') {
+    if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
-	  }
-	  this._name = name;
+    }
+    this._name = name;
   }
 
   get name() {
-	  return this._name;
+    return this._name;
   }
 
   /**
-	 * @param {Number} length
-	 */
+    * @param {Number} length
+     */
   set length(length) {
-	  if (typeof length !== 'number') {
+    if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
-	  }
-	  this._length = length;
+    }
+    this._length = length;
   }
 
   get length() {
-	  return this._length;
+    return this._length;
   }
 
   /**
-	 * @param {Array} students
-	 */
+@param {Array} students
+*/
   set students(students) {
-	  if (students instanceof Array) {
+    if (students instanceof Array) {
       this._students = students;
-	  } else {
+    } else {
       throw new TypeError('Students must be an Array');
-	  }
+    }
   }
 
   get students() {
-	  return this._students;
+    return this._students;
   }
 }
 
