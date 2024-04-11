fb39b1db523e46aeaf88ad0c5ac2746d6f179fd7
aboubakr <c.aboubakr@gmail.com>
2024-04-11 23:22:44 +0000

    update the file 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,27 +1,27 @@
 class Building {
   constructor(sqft) {
-	  if (this.constructor !== Building) {
+    if (this.constructor !== Building) {
       if (typeof this.evacuationWarningMessage !== 'function') {
-		  throw new Error(
+        throw new Error(
           'Class extending Building must override evacuationWarningMessage',
-		  );
+        );
       }
-	  }
-	  this.sqft = sqft;
+    }
+    this.sqft = sqft;
   }
 
   /**
-	 * @param {Number} sqft
-	 */
+* @param {Number} sqft
+*/
   set sqft(sqft) {
-	  if (typeof sqft !== 'number') {
+    if (typeof sqft !== 'number') {
       throw new TypeError('sqft nust be a number');
-	  }
-	  this._sqft = sqft;
+    }
+    this._sqft = sqft;
   }
 
   get sqft() {
-	  return this._sqft;
+    return this._sqft;
   }
 }
 
