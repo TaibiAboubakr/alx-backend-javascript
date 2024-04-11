dd5fba984ae91fa00ac58b749f30672b8be13674
aboubakr <c.aboubakr@gmail.com>
2024-04-11 23:25:26 +0000

    update the file 8-hbtn_class.js

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,32 +1,32 @@
 export default class HolbertonClass {
   constructor(size, location) {
-	  this.size = size;
-	  this.location = location;
+    this.size = size;
+    this.location = location;
   }
 
   get size() {
-	  return this._size;
+    return this._size;
   }
 
   set size(value) {
-	  this._size = value;
+    this._size = value;
   }
 
   get location() {
-	  return this._location;
+    return this._location;
   }
 
   set location(value) {
-	  this._location = value;
+    this._location = value;
   }
 
   [Symbol.toPrimitive](hint) {
-	  if (hint === 'number') {
+    if (hint === 'number') {
       return this.size;
-	  }
-	  if (hint === 'string') {
+    }
+    if (hint === 'string') {
       return this.location;
-	  }
-	  return this;
+    }
+    return this;
   }
 }
