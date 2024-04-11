a65cfd7b1c4942ad399ba0348e869d5ce9610d1b
aboubakr <c.aboubakr@gmail.com>
2024-04-11 23:25:34 +0000

    update the file 10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,41 +1,41 @@
 export default class Car {
   constructor(brand, motor, color) {
-	  this.brand = brand;
-	  this.motor = motor;
-	  this.color = color;
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
   }
 
   get brand() {
-	  return this._brand;
+    return this._brand;
   }
 
   set brand(value) {
-	  this._brand = value;
+    this._brand = value;
   }
 
   get motor() {
-	  return this._motor;
+    return this._motor;
   }
 
   set motor(value) {
-	  this._motor = value;
+    this._motor = value;
   }
 
   get color() {
-	  return this._color;
+    return this._color;
   }
 
   set color(value) {
-	  this._color = value;
+    this._color = value;
   }
 
   static get [Symbol.species]() {
-	  return this;
+    return this;
   }
 
   cloneCar() {
-	  const Species = this.constructor[Symbol.species];
+    const Species = this.constructor[Symbol.species];
 
-	  return new Species();
+    return new Species();
   }
 }
