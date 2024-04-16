1bd856af643b3236a32597c05ccf94a9562803f5
aboubakr <c.aboubakr@gmail.com>
2024-04-16 23:22:10 +0100

    update the file 7-has_array_values.js

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+export default function hasValuesFromArray(set, array) {
+  for (const element of array) {
+    if (!set.has(element)) {
+      return false;
+    }
+  }
+  return true;
+}
