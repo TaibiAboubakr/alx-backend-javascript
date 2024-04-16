185221212cb9fa76841e8e554e1fc0ddd03e5213
aboubakr <c.aboubakr@gmail.com>
2024-04-16 23:37:28 +0100

    add the file 9-groceries_list.js

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,7 @@
+export default function groceriesList() {
+  const GroceriesList = new Map();
+  GroceriesList.set('Apples', 10).set('Tomatoes', 10).set('Pasta', 1).set('Rice', 1)
+    .set('Banana', 5);
+
+  return GroceriesList;
+}
