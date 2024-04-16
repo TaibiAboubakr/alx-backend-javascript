de88822a0bff07f43c0cd4a0a0ab7dcf621f63f8
aboubakr <c.aboubakr@gmail.com>
2024-04-16 23:30:38 +0100

    add the file 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,5 @@
-#!/usr/bin/node
 export default function cleanSet(set, startString) {
-  let string;
-  for (const element of set) {
-    if (element.startsWith(startString)) {
-      string += element;
-    }
-  }
-  return string;
+  const filteredValues = Array.from(set).filter((value) => value.startsWith(startString));
+  const resultString = filteredValues.join('-');
+  return resultString;
 }
