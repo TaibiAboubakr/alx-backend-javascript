2ecbdbe7a83d8bbc14ddd089cb12ddb7b615a62b
aboubakr <c.aboubakr@gmail.com>
2024-04-16 23:08:52 +0100

    update the file 3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 export default function getStudentIdsSum(myArray) {
   if (myArray instanceof Array) {
-    const sum = myArray.reduce((accumulator, student) => accumulator.id || accumulator + student.id, 0);
+    const sum = myArray.reduce(
+      (accumulator, student) => accumulator.id || accumulator + student.id, 0,
+    );
     return (sum);
   }
   return (0);
