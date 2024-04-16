c938a7f17ac377aa159c20287de401376da6e442
aboubakr <c.aboubakr@gmail.com>
2024-04-16 22:52:03 +0100

    update the file 1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 export default function getListStudentsIds(myArray) {
-  if (myArray.isArray) {
+  if (myArray instanceof Array) {
     const arrayIds = myArray.map((student) => student.id);
     return (arrayIds);
   }
