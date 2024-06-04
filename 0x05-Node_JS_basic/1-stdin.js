f6beac6bc1efc5f68c76316ab99ff9c68c4d5ee3
aboubakr <c.aboubakr@gmail.com>
2024-06-04 13:50:29 +0100

    update the file 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,17 @@
 #!/usr/bin/node
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-console.log('Welcome to Holberton School, what is your name?');
-
-process.stdin.on('data', (data) => {
-  const input = data.toString().trim();
-  console.log(`Your name is: ${input}`);
-  console.log('This important software is now closing');
-
-  process.exit();
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
