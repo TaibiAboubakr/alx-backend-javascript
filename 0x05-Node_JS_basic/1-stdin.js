6d47d380fc7bdd737b280cee419373d618c457c9
aboubakr <c.aboubakr@gmail.com>
2024-06-04 12:55:49 +0100

    update the file 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,6 +5,7 @@ const readline = require('readline');
 
 const iface = readline.createInterface({
   input: process.stdin,
+  output: process.stdout,
 });
 
 console.log('Welcome to Holberton School, what is your name?');
